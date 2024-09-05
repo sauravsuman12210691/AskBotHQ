0ebaebc4daf67fc00c6631b0bbe62a84f270368f
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-09-05 14:24:49 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import HomePage from './Component/HomePage';
+import AboutUs from './Component/AboutUs';
+import Navbar from './Component/Navbar';
+import LogIn from './Component/LogIn';
+import SignUp from './Component/SignUp';
+import Contact from './Component/Contact'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const navbarRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <>
+        <Navbar /> <HomePage />
+      </>
+    },
+    {
+      path: "/about",
+      element: <><Navbar /><AboutUs /></>
+    },
+    {
+      path: "/contact",
+      element: <><Navbar /><Contact /></>
+    },
+    {
+      path: '/login',
+      element: <LogIn />
+    },
+    {
+      path: '/signup',
+      element: <SignUp />
+    }
+  ])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={navbarRouter} />
     </>
-  )
+  );
 }
 
 export default App
---
+++ client/src/Component/LogIn.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import '../CSS/LogIn.css';
+import { Link } from "react-router-dom";
+import Logo from '../assets/logo.png';
+
+export default function LogIn() {
+    return (
+        <>
+            <div className="LogInPage">
+                <header>
+                    <Link to="/"><img src={Logo} alt="logo" class="logo" /></Link>
+                </header>
+                <div className="overall">
+                    <div className="socialmedia">
+                        <div className="text" id="socmed">Login using social media account to get quick access</div>
+                        <div className="button">
+                            <button className="but" id="facebook">
+                                <a href="#" className="link facebook">Continue with Facebook</a>
+                            </button>
+                            <button className="but" id="google">
+                                <a href="#" className="link google">Continue with Google</a>
+                            </button>
+                            <button className="but" id="apple">
+                                <a href="#" className="link apple">Continue with Apple</a>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="log">
+                        <div className="text">Login to your account</div>
+                        <div className="text">Don't have an account? <Link to='/signup' className="sign">Sign up for free!</Link></div>
+                        <form>
+                            <label for="email" id="Username" className="required">Username</label>
+                            <input type="email" name="email" id="email" placeholder="Enter Username" required />
+                            <label for="password" className="required">Password</label>
+                            <input type="password" name="password" id="password" required placeholder="Enter Password" />
+                            <button className="submit" id="logIn">Log In</button>
+                        </form>
+                        <button className="submit" id="forpass">Forgot Password</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
